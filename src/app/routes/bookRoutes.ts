41d3fd4e898b27c443257e51afc64a6740b950ff
Rokon Khan <rokonjust@gmail.com>
2025-06-22 21:48:05 +0600

    updated method fix

--- src/app/routes/bookRoutes.ts
+++ src/app/routes/bookRoutes.ts
@@ -12,7 +12,7 @@ const router = express.Router();
 router.post("/", createBook);
 router.get("/", getAllBooks);
 router.get("/:bookId", getBookById);
-router.put("/:bookId", updateBook);
+router.patch("/:bookId", updateBook);
 router.delete("/:bookId", deleteBook);
 
 export default router;
