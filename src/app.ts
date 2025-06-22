4e6581056fc00464d6f985b11afc22c23ec65b88
Rokon Khan <rokonjust@gmail.com>
2025-06-22 16:04:15 +0600

    Connected to MongoDB SuccessFully

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,8 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from "express";
 const app: Application = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to the Express App!");
+});
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,8 @@
+import dotenv from "dotenv";
 import { Server } from "http";
+import mongoose from "mongoose";
 import app from "./app";
+dotenv.config();
 
 let server: Server;
 
@@ -7,6 +10,12 @@ const PORT = 5000;
 
 async function main() {
   try {
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hwao6.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+    );
+
+    console.log("Connected to MongoDB");
+
     server = app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
